09cd850cf1ef0a12688a54d95257fd42c7d517aa
Rayane Marques <mmarquesraioane586@gmail.com>
2024-10-17 02:04:11 -0300

    feat: include auth for animal routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,12 +2,13 @@ import express, { Router } from 'express'
 import userRoutes from './user'
 import animalRoutes from './animal'
 import specieRoutes from './specie'
+import { isAuthenticated } from '@middlewares/isAuthenticated'
 
 const routes: Router = express.Router()
 
 routes
   .use('/', userRoutes)
-  .use('/', animalRoutes)
+  .use('/', isAuthenticated(), animalRoutes)
   .use('/', specieRoutes)
 
 // -- ROTA DE LOGIN
