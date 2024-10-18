38633573a6ffa9865bc6133376b48f5e5b583e30
Rayane Marques <mmarquesraioane586@gmail.com>
2024-10-18 12:16:34 -0300

    feat: return url of file in list

--- src/services/animal/ListAnimalService.ts
+++ src/services/animal/ListAnimalService.ts
@@ -1,9 +1,15 @@
+import { utapi } from "uploadthing";
 import prismaClient from "../../prisma";
 
 export class ListAnimalsService{
   async execute(){
     const animals = await prismaClient.animal.findMany()
 
-    return animals
+    const fileIds = animals.flatMap(a => a.photoAnimal);
+    const files = await utapi.getFileUrls(fileIds);
+    const animalsWithFile = animals.map(a => { return { ...a, photoAnimal: files.data.find(f => f.key === a.photoAnimal)?.url } })
+    
+
+    return animalsWithFile
   }
 }
